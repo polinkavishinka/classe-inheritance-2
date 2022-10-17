05a77c9e336c10cfb0d00938345d13fb080056e4
Polina Vyazankina <polinkavishinka@mail.ru>
2022-10-17 22:53:35 +0300

    update

--- js/Character.js
+++ js/Character.js
@@ -1,29 +1,62 @@
-
 export default class Character {
-    constructor(name,type){
-        this.health = 100;
-        this.level = 1;
-        const types = [
-            'Bowman',
-            'Swordsman',
-            'Magician',
-            'Daemon',
-            'Undead',
-            'Zombie'
-        ];
+  constructor(name, type) {
+    this.health = 100;
+    this.level = 1;
+    const types = [
+      'Bowman',
+      'Swordsman',
+      'Magician',
+      'Daemon',
+      'Undead',
+      'Zombie',
+    ];
 
-        if (types.indexOf(type) === -1){
-            throw new Error ('такого класса нет');
-        } else{
-            this.type = type;
-        }
-
-        if(name.length < 2 || name.length > 10 ){
-            throw new Error ('Ошибка:должна быть больше двух и не более десяти символов включительно');
-        } else{
-            this.name = name;
-        }
+    if (types.indexOf(type) === -1) {
+      throw new Error('такого класса нет');
+    } else {
+      this.type = type;
     }
 
-}
+    if (name.length < 2 || name.length > 10) {
+      throw new Error(
+        'Ошибка:должна быть больше двух и не более десяти символов включительно'
+      );
+    } else {
+      this.name = name;
+    }
+  }
+  // levelUp() {
+  //   if (this.health < 0) {
+  //     throw new Error('нельзя повысить левел умершего');
+  //   } else {
+  //     this.level += 1;
+  //     this.attack += (this.attack / 100) * 20;
+  //     this.defence += (this.defence / 100) * 20;
+  //     this.health = 100;
+  //   }
+  // }
 
+  levelUp() {
+    if (this.health > 0) {
+      this.level += 1;
+      this.attack += (this.attack / 100) * 20;
+      this.defence += (this.defence / 100) * 20;
+      this.health = 100;
+    } else {
+      throw new Error('Нельзя повысить левел умершего');
+    }
+  }
+
+   // damage(points) {
+  //   if (this.health <= 0) {
+  //     this.health = 0;
+  //   }
+  //   this.health -= points * (1 - this.defence / 100);
+  // }
+  damage(points) {
+    this.health -= points * (1 - this.defence / 100);
+    if (this.health < 0) {
+      this.health = 0;
+    }
+  }
+}
--- js/__tests__/Character.test.js
+++ js/__tests__/Character.test.js
@@ -1,45 +1,81 @@
+import Character from '../Character';
 
-import Character from "../Character";
-test ('Создание нового персонажа,ошибок нет',()=>{
+test('Создание нового персонажа,ошибок нет', () => {
+  const person = new Character('Вася', 'Magician');
 
-    const person = new Character ('Вася', 'Magician');
-
-    const result ={
-        name: 'Вася',
-        type: 'Magician',
-        health: 100,
-        level: 1,
-        
-    };
-    expect(person).toMatchObject(result);  
+  const result = {
+    name: 'Вася',
+    type: 'Magician',
+    health: 100,
+    level: 1,
+  };
+  expect(person).toMatchObject(result);
 });
 
-
-
-test ('Имя от 2 до 10 включительно символов ',()=>{
-    const person =  new Character ('Вася','Magician');
-    expect(person.name).toBe("Вася");
-    
+test('Имя от 2 до 10 включительно символов ', () => {
+  const person = new Character('Вася', 'Magician');
+  expect(person.name).toBe('Вася');
 });
 
-test ('Имя не входит в интервал от 2 до 10 символов включительно, выдается ошибка',()=>{
-    const person = () => new Character ('Я','Magician');
-    expect(person).toThrowError(new Error('Ошибка:должна быть больше двух и не более десяти символов включительно'));
-    
+test('Имя не входит в интервал от 2 до 10 символов включительно, выдается ошибка', () => {
+  const person = () => new Character('Я', 'Magician');
+  expect(person).toThrowError(
+    new Error(
+      'Ошибка:должна быть больше двух и не более десяти символов включительно'
+    )
+  );
 });
 
-test ('Имя не входит в интервал от 2 до 10 символов включительно, выдается ошибка',()=>{
-    const person = () => new Character ('Даздраберман','Magician');
-    expect(person).toThrowError(new Error('Ошибка:должна быть больше двух и не более десяти символов включительно'));
-    
+test('Имя не входит в интервал от 2 до 10 символов включительно, выдается ошибка', () => {
+  const person = () => new Character('Даздраберман', 'Magician');
+  expect(person).toThrowError(
+    new Error(
+      'Ошибка:должна быть больше двух и не более десяти символов включительно'
+    )
+  );
 });
-test ('Данный тип присутсвует ',()=>{
-    const person = new Character ('Вася','Magician');
-    expect(person.type).toBe('Magician');
-    
+
+test('Данный тип присутсвует ', () => {
+  const person = new Character('Вася', 'Magician');
+  expect(person.type).toBe('Magician');
 });
-test ('Данный тип отсутсвует ',()=>{
-    const person = () => new Character ('Вася','Ogr');
-    expect(person).toThrowError(new Error('такого класса нет'));
-    
+
+test('Данный тип отсутсвует ', () => {
+  const person = () => new Character('Вася', 'Ogr');
+  expect(person).toThrowError(new Error('такого класса нет'));
+});
+
+// 2 задание
+
+test('Уровень здоровья 0,ошибка, нельзя повысить уровень ', () => {
+  const person = new Character('Вася', 'Magician');
+  person.health = 0;
+  const result = () => person.levelUp();
+  expect(result).toThrow();
+});
+
+test('Уровень здоровья в норме левел повышается', () => {
+  const person = new Character('Вася', 'Magician');
+  person.attack = 10;
+  person.defence = 10;
+  person.levelUp();
+  //console.log(10 + (10 / 100) * 20);
+  expect(person.health).toBe(100);
+  expect(person.attack).toBe(12);
+  expect(person.defence).toBe(12);
+  expect(person.level).toBe(2);
+});
+
+test('Cтандартный урон', () => {
+  const person = new Character('Noah', 'Magician');
+  person.defence = 10;
+  person.damage(40);
+  expect(person.health).toBe(64);
+});
+
+test('Повышенный урон', () => {
+  const person = new Character('Noah', 'Magician');
+  person.defence = 10;
+  person.damage(700);
+  expect(person.health).toBe(0);
 });
